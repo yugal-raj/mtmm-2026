6ec892e3826d9307bd6d20e3c7cbeda90f431586
yugal-raj <yugalrajdeka@gmail.com>
2026-01-05 18:07:59 +0530

    Textual Change - Accommodation

--- src/components/Accomodation.tsx
+++ src/components/Accomodation.tsx
@@ -1,6 +1,30 @@
 import AboutCollageHeader from "./CollageHeader"
 
 export default function Accomodation() {
+  const hotelData = [
+        { 
+            name: "The Greenwood Hotel, Tezpur", 
+            website: (<a href="https://www.thegreenwoodhotels.com" 
+            style={{color:"#3737ff"}}>
+              https://www.thegreenwoodhotels.com
+            </a>) 
+        },
+        { 
+            name: "Cygnett Inn Imperial, Tezpur",
+            website: (<a href="https://www.cygnetthotels.com/cygnett-inn-imperial-tezpur/unit-overview" 
+            style={{color:"#3737ff"}}>
+              https://www.cygnetthotels.com/cygnett-inn-imperial-tezpur/unit-overview
+            </a>)
+        },
+        { 
+            name: "KRC Hotel, Tezpur", 
+             website: (<a href="http://www.krchotels.com" 
+            style={{color:"#3737ff"}}>
+              http://www.krchotels.com
+            </a>)
+        },
+        { name: "Heritage Tezpur", website: "" }
+    ];
   return (
     <section className="relative flex flex-col justify-center">
     <div className="w-full flex items-center justify-center">
@@ -14,27 +38,55 @@ export default function Accomodation() {
         >
             <h2 className="text-[30px] font-bold text-center"
                 style={{"color":"#2364b7"}}
-            >For Faculty and Scientists</h2>
+            >Accommodation</h2>
             <p className="pt-2 text-justify" 
                 style={{"color":"black"}}
-            >Comfortable accommodation will be provided on a chargeable basis in University Guest
-Houses and select hotels located near the Tezpur University campus. Further details
-regarding the arrangements will be communicated in due course.</p>
+            >A limited number of comfortable accommodations is available on a chargeable basis in
+Tezpur University Guest House and will be allotted on a first-come, first serve basis.</p>
+             <p className="pt-4 text-justify" 
+                style={{"color":"black"}}>
+                  Following hotels located in Tezpur are recommended:
+                </p>
         </div>
       </div>
-      <div className="w-full flex items-center justify-center pt-15">
+      <div className="w-full flex items-center justify-center pt-3">
         <div className="w-[80vw]"
             style={{
                 maxWidth:"1280px"
             }}
         >
-            <h2 className="text-[30px] font-bold text-center"
-                style={{"color":"#2364b7"}}
-            >For PhD Students/ PostDocs</h2>
-            <p className="pt-2 text-justify" 
-                style={{"color":"black"}}
-            >(Per day for PhD Students)<br/>(Per day for Postdocs Students)  
-            </p>
+              <table className="min-w-full border border-gray-200 rounded-lg">
+                    <thead
+                    style={{backgroundColor: "#b5dcff"}}
+                    >
+                        <tr>
+                            <th className="px-6 py-3 text-center text-sm font-semibold text-gray-700">
+                            Sl No.
+                            </th>
+                            <th className="px-6 py-3 text-center text-sm font-semibold text-gray-700">
+                            Hotel Name
+                            </th>
+                            <th className="px-6 py-3 text-center text-sm font-semibold text-gray-700">
+                            Website
+                            </th>
+                        </tr>
+                    </thead>
+                                    <tbody className="divide-y divide-gray-200 text-center">
+                    {hotelData.map((item, index) => (
+                        <tr key={index} className="hover:bg-gray-50">
+                        <td className="px-6 py-4 text-gray-800">
+                            {index+1}
+                        </td>
+                        <td className="px-6 py-4 text-gray-800">
+                            {item.name}
+                        </td>
+                        <td className="px-6 py-4 text-gray-600">
+                            <>{item.website}</>
+                        </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
         </div>
       </div>
     </section>
