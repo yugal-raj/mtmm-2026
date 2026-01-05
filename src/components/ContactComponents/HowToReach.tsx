277abf6e869b9a09f1d58912362e5e3d716ee67f
yugal-raj <yugalrajdeka@gmail.com>
2026-01-05 17:43:43 +0530

    Textual Change - How to Reach

--- src/components/ContactComponents/HowToReach.tsx
+++ src/components/ContactComponents/HowToReach.tsx
@@ -10,16 +10,37 @@ export default function HowToReach(){
             <h2 className="text-[30px] font-bold text-center"
                 style={{"color":"#2364b7"}}
             >How to Reach</h2>
-            <p className="pt-2 text-justify" 
+            <h4 className="text-[20px] pt-3"
+                style={{
+                    color:"#8566f6ff"
+                }}
+            >Tezput Airport, Tezpur</h4>
+            <p className="pt-1 text-justify" 
                 style={{"color":"black"}}
-            >Tezpur University is located approximately 300 meters from National Highway 715, ensuring
-easy accessibility. Tezpur University is conveniently accessible via Guwahati, which has
-Lokpriya Gopinath Bordoloi International Airport, well connected to major cities in India and
-abroad through connecting flights. From Guwahati Airport, Tezpur University is
-approximately 190 km away and can be reached in about 4 hours by road via taxi/bus,
-offering a comfortable journey through scenic Assam. Alternatively, Guwahati Railway
-Station is well connected to Tezpur, and onward travel by taxi from Tezpur town to the
-university campus takes around 30 minutes, ensuring smooth and convenient access.</p>
+            >Tezpur Airport is currently under renovation and modernization. Civilian air connectivity is
+expected to resume from April 2026 onwards. Tezpur University campus can be reached by a
+short 15-minute drive by road from the Tezpur Airport.</p>
+                <h4 className="text-[20px] pt-3"
+                style={{
+                    color:"#8566f6ff"
+                }}
+            >Donyi Polo Airport, Itanagar (Hollongi Airport)</h4>
+            <p className="pt-1 text-justify" 
+                style={{"color":"black"}}
+            >The Hollongi Airport at Itanagar is well connected to major international airports (Delhi,
+Mumbai &amp; Kolkata). Hollongi Airport is 110 km away from Tezpur University and due to
+excellent road condition as well as minimal traffic on this route, this journey usually takes
+less than 2 hours.</p>
+                <h4 className="text-[20px] pt-3"
+                style={{
+                    color:"#8566f6ff"
+                }}
+            >Guwahati International Airport (Lokpriya Gopinath Bordoloi International Airport)</h4>
+            <p className="pt-1 text-justify" 
+                style={{"color":"black"}}
+            >Tezpur University is approximately 190 km away from Guwahati International Airport.
+Modern highway connectivity is available on this route and this drive usually takes around 4
+hours.</p>
         </div>
     )
 }
